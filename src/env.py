96320ea200c1fbf65003e3a5ac4245e07729c395
Gustavo <gustavo.reis2@discente.ufg.br>
2024-04-11 11:24:15 -0300

    Criando custom env

---
+++ src/env.py
@@ -0,0 +1,18 @@
+import gym
+from gym import spaces
+import numpy as np
+
+class TrucoEnv(gym.env):
+    def __init__(self):
+        # define your environment
+        # action space, observation space
+        pass
+    def step(self, action):
+        # take some action
+        pass
+    def render(self, mode="dog"):
+        # render your environment (can be a visualisation/print)
+        pass
+    def reset(self):
+        # reset your environment
+        pass
